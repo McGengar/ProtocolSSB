9bd69c9cb724034f9ea878639dc41ab45292327c
azupnik <126894640+azupnik@users.noreply.github.com>
2025-03-01 04:25:23 +0100

    kaczka
    kaczka

--- scripts/ducks.gd
+++ scripts/ducks.gd
@@ -1,47 +1,26 @@
-extends StaticBody2D
-var in_tip = false
-var in_front = false
-var in_back = false
+extends Area2D
+
 
 @onready var car: RigidBody2D = $"../Car"
+var can_move = false  
+var SPEED = 20
+
+
+func _process(delta):
+	if can_move : 
+		position.x += delta*SPEED
+	#if can_move:
+		
+		
+	
+func _on_body_entered(body: Node2D) -> void:
+	if body == car :
+		car.knocked_back(Vector2(0,1)*30000)
+	
+		
 
 
 
-
-
-func _on_sprite_2d_2_has_fallen() -> void:
-	if in_tip == true:
-		car.knocked_back(Vector2(-1,0)*10000)
-	if in_back == true:
-		car.knocked_back(Vector2(0,1)*10000)
-	if in_front == true:
-		car.knocked_back(Vector2(0,-1)*10000)
-
-
-func _on_areatip_body_entered(body: Node2D) -> void:
-	if body == car:
-		in_tip = true
-
-
-func _on_areatip_body_exited(body: Node2D) -> void:
-	if body == car:
-		in_tip = false
-
-
-func _on_areafront_body_entered(body: Node2D) -> void:
-	if body == car:
-		in_front = true
-
-func _on_areafront_body_exited(body: Node2D) -> void:
-	if body == car:
-		in_front = false
-
-
-func _on_areaback_body_entered(body: Node2D) -> void:
-	if body == car:
-		in_back = true
-
-
-func _on_areaback_body_exited(body: Node2D) -> void:
-	if body == car:
-		in_back = false
+func _on_vision_box_body_entered(body: Node2D) -> void:
+	if body == car :
+		can_move = true 
