2654de5e09c3dfcfccfba66c43e14d62b9012ba3
skr8it <unrankedop@gmail.com>
2025-03-01 20:16:22 +0100

    bizony!!!!

--- scripts/rigid_bison.gd
+++ scripts/rigid_bison.gd
@@ -1,24 +1,31 @@
-extends StaticBody2D
+extends RigidBody2D
 var entered = false
 var bison_speed = 0.01
 # Called when the node enters the scene tree for the first time.
+var sigmax = 1
+var sigmay = 1
+@onready var car: RigidBody2D = $"../Car"
+
 func _ready() -> void:
-	pass # Replace with function body.
+	if rotation > 90:
+		sigmax = 0
+		sigmay = -1
+	elif rotation > 0:
+		sigmax = -1
+		sigmay = 0
+	elif rotation == 0:
+		sigmax= 0
+		sigmay = 1
+	elif rotation < 0:
+		sigmax = 1
+		sigmay = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if not entered:
-		for i in range(0,300):
-			entered = true
-			await get_tree().create_timer(bison_speed).timeout
-			if rotation == 0:
-				position.x += 1
-			elif rotation == deg_to_rad(-90):
-				position.y += 1
-			elif rotation == deg_to_rad(90):
-				position.y -= 1
+	if body == car:
+		if not entered:
+			for i in range(0,300):
+				apply_central_force(Vector2(sigmax,sigmay)*100)
